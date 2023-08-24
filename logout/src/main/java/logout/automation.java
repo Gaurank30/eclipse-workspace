5e70e828b66e75167d371cf4e864866d5230cbdb
Gaurank <gaurankzerbade5159@gmail.com>
2023-08-24 16:25:04 +0530

    changes add

--- logout/src/main/java/logout/automation.java
+++ logout/src/main/java/logout/automation.java
@@ -1,10 +1,22 @@
 package logout;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class automation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		WebDriver wb = new ChromeDriver();
+		wb.manage().window().maximize();
+		wb.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		wb.get("");
 
+		wb.findElement(By.xpath("")).click();
 	}
 
 }
